b27038bbf77aa75a7d2c2ad76cd4f9ed32931be3
mohitjain2605 <mohit.jain2@shell.com>
2025-04-05 16:57:04 +0530

    adding latest notes

--- Akshay_Saini/codes-experiments/script.js
+++ Akshay_Saini/codes-experiments/script.js
@@ -1,11 +1,9 @@
-var x = 10;
-console.log(x);
-console.log(window.x);
-console.log(this.x);
 
-function foo() {
-  var a = 20;
-  console.log(a);
-  console.log(window.a);
+function attachEventListeners(){
+  let count =0
+  document.getElementById("clickMe")
+  .addEventListener("click", function click(){
+    console.log("button clicked", ++count)
+  })
 }
-foo();
+attachEventListeners()
